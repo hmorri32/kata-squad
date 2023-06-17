e0d56d059bfeac525881d9fbcf1ac67768d9c89a
Hugh Morrison <hmorri32@gmail.com>
2023-06-17 15:22:09 -0400

    two crystal balls dafuq

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,5 +1,23 @@
 // hundred story building, where do they break? etc.
 
 export default function twoCrystalBalls(breaks: boolean[]): number {
+  const jumpAmount = Math.sqrt(Math.floor(breaks.length));
 
+  let i = jumpAmount;
+
+  for (; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) {
+      break;
+    }
+  }
+
+  i -= jumpAmount;
+
+  for (let j = 0; j < jumpAmount && i < breaks.length; ++j, ++i) {
+    if (breaks[i]) {
+      return i;
+    }
+  }
+
+  return -1;
 }
